07ac5f745500f20aadbc5a9c76a4ffb05bca4e24
MorgV <erikhan03@mail.ru>
2025-06-22 23:23:17 +0300

    HeaderSelector optimization 0.1

--- Front/src/ui/layout/HeaderSelector/HeaderSelector.tsx
+++ Front/src/ui/layout/HeaderSelector/HeaderSelector.tsx
@@ -1,42 +1,29 @@
-import { Link, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { type FC } from "react";
-import style from "./Header.module.css"
+import style from "./Header.module.css";
+
+const routes = [
+  { path: '/', label: 'CSV Аналитик', icon: "/images/install.svg" },
+  { path: '/generation', label: 'CSV Генератор', icon: "/images/circle.svg" },
+  { path: '/history', label: 'История', icon: "/images/time.png" },
+];
 
 export const HeaderSelector: FC = () => {
-  const location = useLocation();
-
-  const routes = [
-    { path: '/', label: 'CSV Аналитик', href: "/images/install.svg" },
-    { path: '/generation', label: 'CSV Генератор', href: "/images/circle.svg" },
-    { path: '/history', label: 'История', href: "/images/time.png" },
-  ]
-  // Так быть не должно routes
   return (
     <ul className={style.wrapper}>
-      {routes.map(({ path, label, href }) =>
-        label ? (
-          <Selector key={path} path={path} href={href} label={label} isActive={location.pathname === path}
-          />
-        ) : null
-      )}
+      {routes.map(({ path, label, icon }) => (
+        <li key={path}>
+          <NavLink
+            to={path}
+            className={({ isActive }) =>
+              isActive ? `${style.selector} ${style.active}` : style.selector
+            }
+          >
+            <img width="25" height="25" src={icon} alt={`${label} icon`} />
+            {label}
+          </NavLink>
+        </li>
+      ))}
     </ul>
   );
 };
-
-
-type SelectorProps = {
-  path: string;
-  href: string;
-  label: string;
-  isActive: boolean;
-};
-
-export const Selector: FC<SelectorProps> = ({ path, href, label, isActive }) => {
-  return (
-    <Link to={path ?? "/"} className={`${style.selector} ${isActive ? style.active : ''}`} key={path}>
-      <img width="25px" height="25px" src={href} alt="img" />
-      {label}
-    </Link>
-
-  )
-}
